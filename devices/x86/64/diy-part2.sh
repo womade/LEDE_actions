28333bdf48f596e3cd20ec2e15bf2e7f976596ac
楊遠徵 <codes@189.cn>
2022-04-01 18:14:43 +0800

    Update diy-part2.sh

--- devices/x86/64/diy-part2.sh
+++ devices/x86/64/diy-part2.sh
@@ -6,6 +6,3 @@
 # Blog: https://blog.ssss.fun
 # Copyright (c) 1998 中國遠徵 <https://i.ssss.fun>
 #==================================================
-
-# 修复mac80211报错
-# cp -f $GITHUB_WORKSPACE/patch/651-rt2x00-driver-compile-with-kernel-5.15.patch package/kernel/mac80211/patches/rt2x00
